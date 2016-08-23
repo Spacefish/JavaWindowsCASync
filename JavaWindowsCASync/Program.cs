5e8c80bb131173624614a5b3d3870496b31fec79
Timo Witte <timo_witte@agilent.com>
2016-08-23 15:30:56 +0200

    detect all keystores from registry

--- JavaWindowsCASync/Program.cs
+++ JavaWindowsCASync/Program.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 
 namespace JavaWindowsCASync
 {
@@ -9,6 +16,62 @@ namespace JavaWindowsCASync
     {
         static void Main(string[] args)
         {
+            List<string> keyStoreFiles = new List<string>();
+            // find all java installations
+            string[] regSearchKeys = new string[]
+            {
+                @"SOFTWARE\JavaSoft\Java Runtime Environment",
+                @"SOFTWARE\JavaSoft\Java Development Kit",
+                @"SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment",
+                @"SOFTWARE\Wow6432Node\JavaSoft\Java Development Kit",
+            };
+
+            foreach (string regSearchKey in regSearchKeys) {
+                RegistryKey jreReg = Registry.LocalMachine.OpenSubKey(regSearchKey);
+                if (jreReg != null)
+                {
+                    string[] jreVersionsNames = jreReg.GetSubKeyNames();
+                    foreach (string jreVersionName in jreVersionsNames)
+                    {
+                        RegistryKey jreVersionKey = jreReg.OpenSubKey(jreVersionName);
+                        if (jreVersionKey != null)
+                        {
+                            object value = jreVersionKey.GetValue("JavaHome");
+                            if (value is string)
+                            {
+                                string cacertsFile = value as string;
+                                if(regSearchKey.Contains("Development"))
+                                {
+                                    cacertsFile = Path.Combine(cacertsFile, "jre");
+                                }
+                                cacertsFile = Path.Combine(cacertsFile, "lib" + Path.DirectorySeparatorChar + "security" + Path.DirectorySeparatorChar + "cacerts");
+                                if (!keyStoreFiles.Contains(cacertsFile))
+                                {
+                                    keyStoreFiles.Add(cacertsFile);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            string javaHome = "";
+            var store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+            RSACryptoServiceProvider.UseMachineKeyStore = true;
+            store.Open(OpenFlags.ReadOnly | OpenFlags.IncludeArchived);
+            foreach(X509Certificate2 cert in store.Certificates)
+            {
+                byte[] certData = cert.Export(X509ContentType.Cert);
+                string certTempFile = Path.Combine(
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                    cert.Thumbprint + ".cer"
+                );
+                File.WriteAllBytes(certTempFile, certData);
+                Process p = Process.Start(@"C:\Program Files (x86)\Java\jre1.8.0_45\bin\keytool.exe", "-importcert -file " + certTempFile + " -keystore keystore.jks -storepass changeit -alias " + cert.Thumbprint + " -noprompt");
+                p.WaitForExit();
+                Console.WriteLine(cert.Subject);
+            }
+            Console.ReadKey();
         }
     }
 }
